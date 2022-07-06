e3817e213ad13b0d6b769dadb1e2774c85db1412
unknown <thecoderjangir@gmail.com>
2022-07-06 19:00:34 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
-const port = process.env.port
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
