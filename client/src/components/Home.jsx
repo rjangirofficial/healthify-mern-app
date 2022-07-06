794826aca3720e67523964917d845d2b1bdefe8a
unknown <thecoderjangir@gmail.com>
2022-07-06 19:32:43 +0530

    make it better

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Loader from './Assets/Loader'
 
 const Home = () => {
 
-    const [loaderValue, setLoaderValue] = useState(false)
+    const [loaderValue,setLoaderValue] = useState(false)
 
     const [blogs, setBlogs] = useState([])
 
@@ -16,10 +16,10 @@ const Home = () => {
                 "Content-type": "application/json"
             }
         })
-        setBlogs(jsonData)
         setLoaderValue(false)
         const data = await resp
         const jsonData = await data.json()
+        setBlogs(jsonData)
     }
 
     useEffect(() => {
@@ -30,28 +30,27 @@ const Home = () => {
 
         <>
 
-            <div className='dashboard_container home_container'>
-                <Link to='/' className='logo'>Healthify</Link>
+        <div className='dashboard_container home_container'>
+            <Link to='/' className='logo'>Healthify</Link>
 
+        {loaderValue && <Loader/>}
 
-                {
-                    blogs.map((item, index) => {
-                        return (
-                            <div key={index} className="blogs_container">
-                                <h2>{item.title}</h2>
-                                <span>Category - {item.category}</span>
-                                <span className='home_author'>By - {item.author}</span>
-                                <Link to={`/blogs/${item._id}`} className='read_btn'>Read Now</Link>
-                            </div>
-                        )
-                    })
-                }
+        {
+            blogs.map((item, index) => {
+                return (
+                    <div key={index} className="blogs_container">
+                        <h2>{item.title}</h2>
+                        <span>Category - {item.category}</span>
+                        <span className='home_author'>By - {item.author}</span>
+                        <Link to={`/blogs/${item._id}`} className='read_btn'>Read Now</Link>
+                    </div>
+                )
+            })
+        }
 
-                {loaderValue && <Loader />}
-
-            </div>
-        </>
-
+    </div>
+    </>
+        
     );
 }
 
